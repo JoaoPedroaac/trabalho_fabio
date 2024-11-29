2d613b509468033023f2ad0219e43a14e6503371
João Pedro <joao.cintra7@hotmail.com>
2024-11-29 17:42:58 -0300

    Alteração de banco + adição de requisitos.

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
 const Hapi = require("@hapi/hapi");
+const mongoose = require("mongoose"); // Importa o Mongoose
 const routes = require("./config/routes");
 const config = require('./config/envs-config');
-
 
 const server = Hapi.server({
     port: config.port,
     host: config.host
 });
 
-//apresenta as rotas http mapeadas ao hapi
 routes.forEach((path) => server.route(path));
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(config.databaseUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Conexão bem-sucedida com o MongoDB");
+    } catch (err) {
+        console.error("Erro ao conectar ao MongoDB:", err.message);
+        process.exit(1); // Encerra a aplicação se a conexão falhar
+    }
+};
+
+connectDB();
 
 module.exports = server;
